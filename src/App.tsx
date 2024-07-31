9d5e3c77602fd610830179daf64c610664aab08d
goayasushi <goayasushi@gmail.com>
2024-07-31 11:59:02 +0900

    BEとの疎通確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/hello")
+      .then((response) => {
+        setMessage(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the posts!", error);
+      });
+  }, []);
+  return <div className="App">{message}</div>;
 }
 
 export default App;
